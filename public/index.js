a0214bcad30bef621e5d3b21ed1ba6805b81de90
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2024-11-06 14:26:07 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -21,6 +21,23 @@ const error = document.getElementById("uv-error");
 const errorCode = document.getElementById("uv-error-code");
 const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
 
+if(new URLSearchParams(new URL(window.location).search).has("dest")) {
+	try {
+		await registerSW();
+	} catch (err) {
+		error.textContent = "Failed to register service worker.";
+		errorCode.textContent = err.toString();
+		throw err;
+	}
+	const url = search(address.value, searchEngine.value);
+	
+	let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+	if (await connection.getTransport() !== "/epoxy/index.mjs") {
+		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+	}
+	window.location.replace(__uv$config.prefix + __uv$config.encodeUrl(url))
+}
+
 form.addEventListener("submit", async (event) => {
 	event.preventDefault();
 
