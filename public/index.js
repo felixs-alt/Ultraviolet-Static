2c8cfd7fb8a33a1c387ef4d02ad7dbd16cbf7c27
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2024-11-07 08:11:37 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,4 +1,23 @@
 "use strict";
+async function redirect(params) {
+	if(params.has("dest")) {
+	try {
+		await registerSW();
+	} catch (err) {
+		error.textContent = "Failed to register service worker.";
+		errorCode.textContent = err.toString();
+		throw err;
+	}
+	const url = decodeURIComponent(params.get("dest"));
+	
+	let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+	if (await connection.getTransport() !== "/epoxy/index.mjs") {
+		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+	}
+	window.location.replace(__uv$config.prefix + __uv$config.encodeUrl(url))
+	}
+}
+redirect(new URLSearchParams(new URL(window.location).search))
 /**
  * @type {HTMLFormElement}
  */
@@ -20,23 +39,6 @@ const error = document.getElementById("uv-error");
  */
 const errorCode = document.getElementById("uv-error-code");
 const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
-params = new URLSearchParams(new URL(window.location).search)
-if(params.has("dest")) {
-	try {
-		await registerSW();
-	} catch (err) {
-		error.textContent = "Failed to register service worker.";
-		errorCode.textContent = err.toString();
-		throw err;
-	}
-	const url = decodeURIComponent(params.get("dest"));
-	
-	let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
-	if (await connection.getTransport() !== "/epoxy/index.mjs") {
-		await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
-	}
-	window.location.replace(__uv$config.prefix + __uv$config.encodeUrl(url))
-}
 
 form.addEventListener("submit", async (event) => {
 	event.preventDefault();
